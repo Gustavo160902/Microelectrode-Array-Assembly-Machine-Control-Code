b3c99dcc96180c70132ebf673a3d77d8a8fd65d1
Gustavo160902 <153106088+Gustavo160902@users.noreply.github.com>
2026-04-14 11:28:31 -0400

    Update assembly.py

--- assembly.py
+++ assembly.py
@@ -48,8 +48,9 @@ def run_full_assembly():
     time.sleep(20)
 
     # Step 5 — adjust axes slightly (test values)
-    move_linear_stage(x, '+', 5000, wait_for_stop=True, max_wait=30.0)
-    move_linear_stage(y, '+', 5000, wait_for_stop=True, max_wait=30.0)
+    move_linear_stage(x, '-', 5000, wait_for_stop=True, max_wait=30.0)
+    move_linear_stage(y, '-', 5000, wait_for_stop=True, max_wait=30.0)
+    move_linear_stage(z, '+', 5000, wait_for_stop=True, max_wait=30.0)
 
     # Step 6 — rotate +90 back to print station
     move_linear_stage('r', '+', 90, wait_for_stop=True, max_wait=30.0)
